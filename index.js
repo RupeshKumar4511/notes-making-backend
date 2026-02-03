978f2292612d11779bba5d8ecbb86983f8c4b09f
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-03 15:50:13 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import app from './app.js';
+import connection from './config/db.js';
+import {config} from 'dotenv';
+config()
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
